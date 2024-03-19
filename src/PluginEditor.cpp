4f099dddd3af08b7d16c6239b999fee827408b96
Jakub Jankowski <jakub.jankowski@me.com>
2024-03-19 14:32:02 +0100

    feat(PluginEditor): added labels to controls

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -35,6 +35,23 @@ MHVAudioProcessorEditor::MHVAudioProcessorEditor (MHVAudioProcessor& p)
     std::make_unique<ComboboxAttachment>(p.apvts, paramID::irIndex, m_inpulseComboBox);
     addAndMakeVisible(m_inpulseComboBox);
 
+    // Take care of the labels
+    m_inputGainLabel.setText("Input Gain", juce::dontSendNotification);
+    m_inputGainLabel.attachToComponent(&m_inputGainDial, false);
+    addAndMakeVisible(m_inputGainLabel);
+
+    m_outputGainLabel.setText("Output Gain", juce::dontSendNotification);
+    m_outputGainLabel.attachToComponent(&m_outputGainDial, false);
+    addAndMakeVisible(m_outputGainLabel);
+
+    m_dryWetLabel.setText("Dry/Wet Mix", juce::dontSendNotification);
+    m_dryWetLabel.attachToComponent(&m_dryWetSlider, false);
+    addAndMakeVisible(m_dryWetLabel);
+
+    m_inpulseLabel.setText("Impulse Response", juce::dontSendNotification);
+    m_inpulseLabel.attachToComponent(&m_inpulseComboBox, false);
+    addAndMakeVisible(m_inpulseLabel);
+
     // Set the size of the editor
     setSize(JPG_WIDTH, JPG_HEIGHT);
 }
@@ -64,20 +81,20 @@ void MHVAudioProcessorEditor::resized()
     // Don't worry about the magic numbers, they are not reused anywhere else
     // Also, don't worry about the implicit type conversion, it's safe here
     // First, we remove the top 80% of the area
-    area.removeFromTop(area.getHeight() * 0.8);
+    area.removeFromTop(area.getHeight() / 20);
+    auto comboBoxArea = area.removeFromTop(area.getHeight() * 0.8).reduced(border);
     // Then remove 25% of the width from the left and right sides
     auto inputGainArea = area.removeFromLeft(area.getWidth() / 4).reduced(border);
     auto outputGainArea  = area.removeFromRight(area.getWidth() * 0.33);
     // Finally, we remove the top 33% of the middle area
-    area.removeFromTop(area.getHeight() * 0.33);
-    auto sliderArea = area.removeFromTop(area.getHeight() * 0.5).reduced(border);
+    area.removeFromTop(area.getHeight() * 0.66);
     // After the area was removed, we can set the middle area
-    auto middleArea = area.reduced(border);
+    auto sliderArea = area.reduced(border);
     // Now we set the bounds of the components
     m_inputGainDial.setBounds(inputGainArea);
-    m_inpulseComboBox.setBounds(middleArea);
+    m_inpulseComboBox.setBounds(comboBoxArea);
     m_outputGainDial.setBounds(outputGainArea);
     m_dryWetSlider.setBounds(sliderArea);
     // And we set the size of the combobox
-    m_inpulseComboBox.setSize(middleArea.getWidth(), middleArea.getHeight() * 0.9);
+    m_inpulseComboBox.setSize(comboBoxArea.getWidth(), comboBoxArea.getHeight() / 12);
 }
--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -16,6 +16,19 @@ private:
     juce::Slider m_outputGainDial;
     juce::Slider m_dryWetSlider;
     juce::ComboBox m_inpulseComboBox;
+    juce::Label m_inputGainLabel;
+    juce::Label m_outputGainLabel;
+    juce::Label m_dryWetLabel;
+    juce::Label m_inpulseLabel;
+    // Those are only used to make the type names shorters
+    using APTVS = juce::AudioProcessorValueTreeState;
+    using SliderAttachment = APTVS::SliderAttachment;
+    using ComboboxAttachment = APTVS::ComboBoxAttachment;
+    // GUI components' attachments
+    SliderAttachment m_inputGainAttachment;
+    SliderAttachment m_outputGainAttachment;
+    SliderAttachment m_dryWetAttachment;
+    std::unique_ptr<ComboboxAttachment> m_inpulseComboBoxAttachment;
 // Methods
 public:
     explicit MHVAudioProcessorEditor (MHVAudioProcessor&);
@@ -23,14 +36,5 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 private:
-    using APTVS = juce::AudioProcessorValueTreeState;
-    using SliderAttachment = APTVS::SliderAttachment;
-    using ComboboxAttachment = APTVS::ComboBoxAttachment;
-
-    SliderAttachment m_inputGainAttachment;
-    SliderAttachment m_outputGainAttachment;
-    SliderAttachment m_dryWetAttachment;
-    std::unique_ptr<ComboboxAttachment> m_inpulseComboBoxAttachment;
-
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MHVAudioProcessorEditor)
 };
